a5fd27d53ccf7da8911732ba99d520b6479b8e6e
Artem Matok <artemmatok@gmail.com>
2024-04-13 02:16:38 +0300

    Add project files.

---
+++ TestChat/Server/Data/Entities/Message.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestChat.Server.Data.Entities
+{
+    public class Message
+    {
+        public long Id { get; set; }
+        public int FromId { get; set; }
+        public int ToId { get; set; }
+        [Required]
+        public string Content { get; set; }
+        public DateTime AddedOn { get; set; }
+
+
+        public virtual User FromUser { get; set; }
+        public virtual User ToUser { get; set; }
+    }
+}
---
+++ TestChat/Server/Data/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestChat.Server.Data.Entities
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public DateTime AddedOn { get; set; }
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+
+
+}
